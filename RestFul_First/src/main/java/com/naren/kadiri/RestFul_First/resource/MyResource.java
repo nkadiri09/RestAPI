eeea289fa396dd721769850fdf224b1591190614
NarendraReddy <nkadiri09@gmail.com>
2017-09-15 23:09:00 -0400

    @PUT @Delete @Consume @Produce
    All crud operation for in memory Messages

--- RestFul_First/src/main/java/com/naren/kadiri/RestFul_First/resource/MyResource.java
+++ RestFul_First/src/main/java/com/naren/kadiri/RestFul_First/resource/MyResource.java
@@ -3,8 +3,10 @@ package com.naren.kadiri.RestFul_First.resource;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -37,7 +39,7 @@ public class MyResource {
 	@Path("/{messageId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") Long messageId) {
-		return new Message(1, "NarendraTest", "Naren");
+		return msgService.getMessage(messageId);
 	}
 
 	@POST
@@ -45,6 +47,23 @@ public class MyResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Message addMessages(Message message) {
 		return msgService.addMessage(message);
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") int id, Message message) {
+		message.setId(id);
+		return msgService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message deleteMessage(@PathParam("messageId") Long id) {
+		return msgService.deleteMessage(id);
 	}
 
 }
